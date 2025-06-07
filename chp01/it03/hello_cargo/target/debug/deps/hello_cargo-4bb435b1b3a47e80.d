86b1797d836bb337f4fd0083632c70c97b1df082
GabrielDelMonte <gabrieldmsn@hotmail.com>
2025-06-06 22:21:06 -0300

    chp1 and chp2

---
+++ chp01/it03/hello_cargo/target/debug/deps/hello_cargo-4bb435b1b3a47e80.d
@@ -0,0 +1,5 @@
+/home/gabriel/Documents/Estudos/Rust/crab-sandbox/chp01/it03/hello_cargo/target/debug/deps/libhello_cargo-4bb435b1b3a47e80.rmeta: src/main.rs
+
+/home/gabriel/Documents/Estudos/Rust/crab-sandbox/chp01/it03/hello_cargo/target/debug/deps/hello_cargo-4bb435b1b3a47e80.d: src/main.rs
+
+src/main.rs:
---
+++ chp02/guessing_game/target/debug/deps/guessing_game-e060a693e7a16794.d
@@ -0,0 +1,5 @@
+/home/gabriel/Documents/Estudos/Rust/crab-sandbox/chp02/guessing_game/target/debug/deps/guessing_game-e060a693e7a16794: src/main.rs
+
+/home/gabriel/Documents/Estudos/Rust/crab-sandbox/chp02/guessing_game/target/debug/deps/guessing_game-e060a693e7a16794.d: src/main.rs
+
+src/main.rs:
